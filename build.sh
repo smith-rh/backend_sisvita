e7f8ecbc5228d2e91576b3596bcbc716b6d54061
Smith Rodriguez <smith.rodriguez@unmsm.edu.pe>
2024-05-31 22:05:46 -0500

    listo para el despliege3

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
+
+
 python manage.py migrate
